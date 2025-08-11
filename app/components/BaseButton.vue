f8bf1ae1db5de83aa90f156f72b2f2d98bacafce
RonneyGK <gkrohan53@gmail.com>
2025-08-11 12:03:07 +0530

    Button Component created

---
+++ app/components/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :class="[
+      'bg-yellow-400 hover:bg-yellow-500 text-white px-10 py-2.5 rounded-full font-[Open_Sans] transition-all duration-300 mt-10 mx-auto block',
+      extraClasses
+    ]"
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: { type: String, required: true },        // Button text
+  extraClasses: { type: String, default: '' }     // Optional extra Tailwind classes
+});
+
+const emit = defineEmits(['click']);
+</script>
